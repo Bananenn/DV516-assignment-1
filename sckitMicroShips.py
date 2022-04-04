25819645ee7e01d4bd7461629da44f7442262250
Andre <af223kr@student.lnu.se>
2022-04-04 11:10:05 +0200

    Code cleanup

--- sckitMicroShips.py
+++ sckitMicroShips.py
@@ -1,10 +1,8 @@
 from matplotlib.colors import ListedColormap
-from matplotlib.patches import Rectangle
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
 
 # Sources:
 # https://towardsdatascience.com/knn-using-scikit-learn-c6bed765be75
@@ -41,45 +39,6 @@ def perdictPoint(toTest, k):
     toTest = np.reshape(np.array(toTest), (1,-1))
     return knn.predict( toTest )[0]
 
-def perdictMany(toTest, k):
-    knn.n_neighbors = k
-    return np.reshape(knn.predict( toTest ), (1,-1)) 
-
-def improvedPerdictionBoundry(plot):
-    allXYCords = []
-    for x in xRange:
-        for y in yRange:
-            allXYCords.append((x,y))
-
-    allXYCords = np.array(allXYCords)
-    allResults = np.array(perdictMany(allXYCords))
-    cordsWithO = np.append(allXYCords,allResults.T, axis=1)
-    
-    for x,y,ok in cordsWithO:
-        if ok == 0:
-            rec = plt.Rectangle((x,y), stepSize, stepSize, fc='blue', alpha=0.5)
-            plot.add_patch(rec) #Thease are semi transparant rectangels
-        else:
-            rec = plt.Rectangle((x,y), stepSize, stepSize, fc='red', alpha=0.5)
-            plot.add_patch(rec) #Thease are semi transparant rectangels
-    
-def makePerdictionBoundry(plot):
-    # X and Y ranges from -1 to 1.4 found to be good size
-    stepSize = 0.024 # want 100 steps 2.4/100 = 0.024
-    xRange = np.arange(-1.0, 1.4, stepSize)
-    yRange = np.arange(-1.0, 1.4, stepSize)
-
-    for x in xRange:
-        #progress = round(((1+x)/2.4)*100)
-        #if (progress % 10) == 0: print(f"Progress: {progress}%") #Simple progress meter will pring every 10% of progress
-        for y in yRange:
-            if perdictPoint((x,y)) == 0:
-                rec = plt.Rectangle((x,y), stepSize, stepSize, fc='blue', alpha=0.5)
-                plot.add_patch(rec) #Thease are semi transparant rectangels
-            else:
-                rec = plt.Rectangle((x,y), stepSize, stepSize, fc='red', alpha=0.5)
-                plot.add_patch(rec) #Thease are semi transparant rectangels
-
 def plotOrgData(plot):
     for cords,ok in zip(x_val, y_val):
         if ok == 1:
@@ -108,19 +67,10 @@ def checkTrainingError(k):
             errors +=1
     return errors
 
-#15s
-#makePerdictionBoundry(k3plt)
-
-#10s
-#improvedPerdictionBoundry(k3plt)
-
 #Mesh print perdicton
 for plot,k in plots:
     plotOrgData(plot)
     makePerdictionBoundryMesh(plot,k)
     plot.set_title(f'K = {k} Training error: {checkTrainingError(k)}')
     
-
-
-
 plt.show()
